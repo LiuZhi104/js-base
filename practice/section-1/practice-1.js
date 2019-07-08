067433228103df43951859ff4a6ed0ee2430cfa2
JEFFREY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUJE7@OOCL.COM>
2019-07-08 12:06:24 +0800

    Initial commit

---
+++ practice/section-1/practice-1.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function collectSameElements(collectionA, collectionB) {
+            if(collectionA.join()==collectionB.join()){
+              document.write("这两个元素为相同元素")；
+            }else{
+              document.write("不满足为相同元素")；
+            }
+
+
+  return '实现练习要求，并改写该行代码。';
+}
---
+++ practice/section-1/practice-2.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function collectSameElements(collectionA, collectionB) {
+    for(var i=0;i<=collectionA.length;i++){
+          for(var j=0;j<=collectionB.length;j++){
+                if(collectionA[i]==collectionB[j]){
+                  doucment.write("两个元素相同")；
+                  break；
+                }
+                else{
+                 doucment.write("两个元素不相同")；
+                }
+
+          }
+    }
+
+  return '实现练习要求，并改写该行代码。';
+
+}
